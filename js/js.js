b87c8b73f3d6494554605243ce7a033389bd4d02
DoroshenkoYurij <doroshenko_yuriji2017@yahoo.com>
2017-02-18 17:43:23 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+
+    // $("#bottom_img").attr("style","top:" + bottom_img_top + "px");
+
+    $(".jwplayer").attr("style","width:" + "50%;" + "height:450px!important;");
+
+});
+
+$(window).resize(function() {
+	
+});
+    
