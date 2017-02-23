3ba2bc932ad4490907eaf49cc78ee6e87130fceb
DoroshenkoYurij <doroshenko_yuriji2017@yahoo.com>
2017-02-23 15:57:45 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-	
-    $(".jwplayer").attr("style","width:" + "50%;" + "height:450px!important;");
 
+    $(".jwplayer").attr("style","width:" + "50%;" + "height:450px!important;");
+	var playerInstance = jwplayer("player");
+    playerInstance.setup({
+        image: "/uploads/images/1c863b3c6bf90ab5c99110870a529f2e/original.jpg",
+        file: "http://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8",
+        title: "My Cool Trailer"           
+    });
 });
